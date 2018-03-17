da21c9e2bf37ad1b0ee01808472f71077dd9182e
bnoden <brandroid369@gmail.com>
2018-03-17 16:34:42 -0500

    func

--- bitnot.js
+++ bitnot.js
@@ -15,7 +15,7 @@ const { arr1, arr2, arr3 } = objectOfArrays;
 
 show('arr1', arr1); // [ -2, -1, 0, 1, 2, 3, 4, 5 ]
 show('arr2', arr2); // () => arr1.map(bitnot)
-show('arr2', arr2()); // [ 1, 0, -1, -2, -3, -4, -5, -6 ]
+show('arr2()', arr2()); // [ 1, 0, -1, -2, -3, -4, -5, -6 ]
 
 show('arr3', arr3);
 show('arr3.map(bitnot)', arr3.map(bitnot)); // [ 1, 0, -1, -2, -3, -4, -5, -6 ]
